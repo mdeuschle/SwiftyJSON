0cf45725ae23f0f13786f8dc83e6ce347e309e9a
Matt Deuschle <mdeuschle@gmail.com>
2016-05-01 18:07:18 -0700

    testing Alamofire and SwiftyJSON together with API

--- Swifty/ViewController.swift
+++ Swifty/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        Alamofire.request(.GET, "http://dev-hl.sherlockpipeline.com/api/getFilterItems").responseJSON { (response) in
+
+            if let value = response.result.value {
+
+                let json = JSON(value)
+
+                for i in 1...20 {
+
+                    let id = json["\(i)"].dictionaryValue
+
+                    if let height = id["height"]?.stringValue {
+
+                        print(height)
+                    }
+                }
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
